882eda1ea22daa54ee7c310518fee7bfcf4bf799
Reeti Agarwal <reeti05agarwal@gmail.com>
2025-09-20 21:38:04 +0530

    Adding and updaing projects

--- src/constants/ProjectData.ts
+++ src/constants/ProjectData.ts
@@ -1,6 +1,5 @@
 export interface ProjectData {
-  title: string;
-  author: string;
+  title: string; 
   tags: string[];
   description: string;
   image: string;
@@ -10,35 +9,42 @@ export interface ProjectData {
 
 export const projectData: ProjectData[] = [
   {
-    title: "Network Intrusion System",
-    author: "By Hirohiko Araki",
+    title: "TraceProbe",
+    tags: ["Apache", "ElasticSearch", "React", "Docker", "Kafka"],
+    description:
+      "A real-time analytics tool for law enforcement to detect suspicious patterns in massive IPDR logs using Kafka and Flink. Developed for a cybersecurity hackathon to aid in digital forensic investigations.",
+    image: "/images/ipdr.png",
+    readMoreLink: "https://reeti05agarwal.github.io/Tech_Blogs/Projects/TraceProbe",
+    githubLink: "https://github.com/Reeti05Agarwal/TraceProbe",
+  },
+  {
+    title: "NexGaurd",
+    tags: ["Python", "Machine Learning", "Cloud", ],
+    description:
+      "Designed a secure, AI-based system for real-time fraud detection and behavior analysis. Utilized knowledge distillation, time-series RNNs, and explainability tools (LIME) to ensure efficient and transparent decision-making.",
+    image: "/images/cat.png",
+    readMoreLink: "https://example.com/steel-ball-run",
+    githubLink: "https://github.com/Reeti05Agarwal/NexGaurd",
+  },
+  {
+    title: "Network Intrusion System", 
     tags: ["Java", "Security", "Network", "MySQL"],
     description:
       "Developed a Rule-based Network Intrusion Detection (NID) system incorporating real-time packet inspection, slashing network breaches by 15% and flagging 200+ suspicious packetsdaily.",
     image: "/images/cat.png", // store in /public/images,
-    readMoreLink: "https://example.com/steel-ball-run",
+    readMoreLink: "https://reeti05agarwal.github.io/Tech_Blogs/Projects/Network-Intrusion-System-(NIDS)",
     githubLink: "https://github.com/Reeti05Agarwal/NIDS/tree/main",
   },
-  {
-    title: "AI driven Customer Behavior Prediction and Fraud Detection",
-    author: "By Ghostwriter X",
-    tags: ["Python", "Machine Learning", "Cloud", ],
-    description:
-      "Designed a secure, AI-based system for real-time fraud detection and behavior analysis. Utilized knowledge distillation, time-series RNNs, and explainability tools (LIME) to ensure efficient and transparent decision-making.",
-    image: "/images/cat.png",
-    readMoreLink: "https://example.com/steel-ball-run",
-    githubLink: "https://github.com/Reeti05Agarwal/PBL-1",
-  },
-  {
-    title: "AI-Powered Cybersecurity Assistant",
-    author: "By Ghostwriter X",
-    tags: ["Python", "Machine Learning", "Cloud", ],
-    description:
-      "Designed a secure, AI-based system for real-time fraud detection and behavior analysis. Utilized knowledge distillation, time-series RNNs, and explainability tools (LIME) to ensure efficient and transparent decision-making.",
-    image: "/images/cat.png",
-    readMoreLink: "https://example.com/steel-ball-run",
-    githubLink: "",
-  },
+  // {
+  //   title: "AI-Powered Cybersecurity Assistant",
+  //   tags: ["Python", "Machine Learning", "Cloud", ],
+  //   description:
+  //     "Designed a secure, AI-based system for real-time fraud detection and behavior analysis. Utilized knowledge distillation, time-series RNNs, and explainability tools (LIME) to ensure efficient and transparent decision-making.",
+  //   image: "/images/cat.png",
+  //   readMoreLink: "https://example.com/steel-ball-run",
+  //   githubLink: "",
+  // },
+
   // {
   //   title: " ",
   //   author: " ",
