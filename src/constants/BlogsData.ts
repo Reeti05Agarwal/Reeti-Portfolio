1003f4851108a55b007367c0540b58326c31bcb1
maria <maria@maria.maria>
2026-01-14 18:35:40 +0530

    design changes

--- src/constants/BlogsData.ts
+++ src/constants/BlogsData.ts
@@ -13,27 +13,27 @@ export const BlogsData = [
     summary: "",
     date: "15 Dec 2025",
     link: "https://medium.com/@reeti05agarwal/windows-services-architecture-f6b78997467f",
-    image: "/images/blog_winser.png" 
+    image: "./images/blog_winser.png" 
   }, 
   {
     title: "Mastering Nmap",
     summary: "The Ultimate Nmap Guide for Beginners",
     date: "19 Aug 2025",
     link: "https://medium.com/@reeti05agarwal/mastering-nmap-the-ultimate-guide-for-beginners-209d87c124eb",
-    image: "/images/blog_nmap.png" 
+    image: "./images/blog_nmap.png" 
   }, 
   {
     title: "VirtualBox Kali Linux",
     summary: "Step-by-step guide to installing Kali Linux on VirtualBox.",
     date: "11 Aug 2025",
     link: "https://medium.com/@reeti05agarwal/kali-linux-virtual-machine-virtual-box-d6d35de163b6",
-    image: "/images/blog_kali.png" 
+    image: "./images/blog_kali.png" 
   },  
   {
     title: "CTF Reverse Engineering",
     summary: "A Beginner’s Guide with Ghidra and GDB",
     date: "6 Jun 2025",
     link: "https://medium.com/@reeti05agarwal/ctf-reverse-engineering-using-ghidra-and-gdb-eb9b1c682264",
-    image: "/images/blog_steg.webp" 
+    image: "./images/blog_steg.webp" 
   },
 ];
--- src/constants/ProjectData.ts
+++ src/constants/ProjectData.ts
@@ -15,7 +15,7 @@ export const projectData: ProjectData[] = [
     description:
       "A real-time analytics tool for law enforcement to detect suspicious patterns in massive IPDR logs using Kafka and Flink. Developed for a cybersecurity hackathon to aid in digital forensic investigations.",
     short: "A real-time analytics tool for law enforcement to detect suspicious patterns in massive IPDR logs using Kafka and Flink.",
-    image: "/images/ipdr.png",
+    image: "./images/ipdr.png",
     link: "https://reeti05agarwal.github.io/Tech_Blogs/Projects/TraceProbe",
     githubLink: "https://github.com/Reeti05Agarwal/TraceProbe",
   },
@@ -25,7 +25,7 @@ export const projectData: ProjectData[] = [
     description:
       "Designed a secure, AI-based system for real-time fraud detection and behavior analysis. Utilized knowledge distillation, time-series RNNs, and explainability tools (LIME) to ensure efficient and transparent decision-making.",
     short: "AI-based system for real-time fraud detection and behavior analysis.",
-    image: "/images/cat.png",
+    image: "./images/cat.png",
     link: "https://reeti05agarwal.github.io/Tech_Blogs/Projects/NexGaurd",
     githubLink: "https://github.com/Reeti05Agarwal/NexGaurd",
   },
@@ -35,7 +35,7 @@ export const projectData: ProjectData[] = [
     description:
       "Developed a Rule-based Network Intrusion Detection (NID) system incorporating real-time packet inspection, slashing network breaches by 15% and flagging 200+ suspicious packetsdaily.",
     short: "Rule-based Network Intrusion Detection (NID) system incorporating real-time packet inspection.",
-    image: "/images/cat.png", // store in /public/images,
+    image: "./images/cat.png", // store in /public./images,
     link: "https://reeti05agarwal.github.io/Tech_Blogs/Projects/Network-Intrusion-System-(NIDS)",
     githubLink: "https://github.com/Reeti05Agarwal/NIDSs",
   },  
