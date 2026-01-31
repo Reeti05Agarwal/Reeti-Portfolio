ee1860a6cd00417d97535f9c7e0a891054fa5e51
maria <maria@maria.maria>
2026-01-31 10:25:18 +0530

    deleting cert page

--- src/app/certifications/page.tsx
+++ src/app/certifications/page.tsx
@@ -1,27 +1,27 @@
-'use client';
-import { motion } from "framer-motion"; 
-import CertCard from "@/components/Cards/CertCard";  
-import SectionHeading from "@/components/ui/SectionHeading";
+// 'use client';
+// import { motion } from "framer-motion"; 
+// import CertCard from "@/components/Cards/CertCard";  
+// import SectionHeading from "@/components/ui/SectionHeading";
 
 
-export default function ProjectsPage() {
-  return (
-    <main className="px-4 sm:px-8 md:px-16 lg:px-32 xl:px-70 pt-24">
-      <motion.div
-        className="mb-4"
-      >
+// export default function ProjectsPage() {
+//   return (
+//     <main className="px-4 sm:px-8 md:px-16 lg:px-32 xl:px-70 pt-24">
+//       <motion.div
+//         className="mb-4"
+//       >
      
-        <div className="mb-8 group">
-          <SectionHeading 
-              directory="reeti@root:~$"
-              command="ls Certifications"
-              description="Explore my technical arsenal"
-              glowColor="cyan"
-          />   
-        </div>  
-      </motion.div>
-      <CertCard />
+//         <div className="mb-8 group">
+//           <SectionHeading 
+//               directory="reeti@root:~$"
+//               command="ls Certifications"
+//               description="Explore my technical arsenal"
+//               glowColor="cyan"
+//           />   
+//         </div>  
+//       </motion.div>
+//       <CertCard />
  
-    </main>
-  );
-}
+//     </main>
+//   );
+// }
